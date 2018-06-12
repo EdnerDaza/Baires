a380bdcb00b4ef034b9ce06b24086fed5c54ab01
Admin <admin@MacBook-Pro-de-Admin.local>
2018-06-12 15:40:58 -0500

    Initial commit

---
+++ app/src/main/java/com/daza/edner/myapplicationbaires/MainActivity.java
@@ -0,0 +1,16 @@
+package com.daza.edner.myapplicationbaires;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
